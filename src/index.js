73f48927966fd6e8885b2a7fe5902eb5a52a3196
AlejandraKeber <alejandrakeber@gmail.com>
2022-09-27 23:51:30 -0500

    Add html css and js files

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import _ from 'lodash';
+import _ from 'lodash';  // eslint-disable-line
 import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
 
-function component() {
-    const element = document.createElement('div');
+const taskList = document.querySelector('.task-list');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+const taskArr = [
+  {
+    index: 1,
+    description: 'Task 1 ...............',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'Task 2 ...............',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Task 3 ...............',
+    completed: false,
+  },
+  {
+    index: 4,
+    description: 'Task 4 ..............',
+    completed: false,
+  },
+];
 
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    console.log(Notes);
-
-    return element;
-}
-
-document.body.appendChild(component());
+taskList.innerHTML = taskArr.map((task) => `<li id="task-${task.index}" class="task-item">
+  <input type="checkbox" class="checkbox">
+  <span class="task-text">${task.description}</span>
+  <button class="delete">X</button>
+</li>`).join('');
