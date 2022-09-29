70a3b9de8887f731fdc3fda24cebf3c8ba7729d1
AlejandraKeber <alejandrakeber@gmail.com>
2022-09-28 22:41:44 -0500

    Add modules and js code

---
+++ src/modules/2add.js
@@ -0,0 +1,13 @@
+import Task from './1task.js';
+
+export default function addTask(task) {
+  const parsedArr = JSON.parse(localStorage.getItem('taskArr')) || [];
+  const counter = parsedArr.length + 1;
+  const newTask = new Task(counter, task);
+
+  parsedArr.push(newTask);
+  localStorage.setItem('taskArr', JSON.stringify(parsedArr));
+
+  const taskfield = document.querySelector('.text-field');
+  taskfield.value = '';
+}
