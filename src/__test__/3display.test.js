50cee010a9c4fdaa8d8289f4d5469647572d840e
AlejandraKeber <alejandrakeber@gmail.com>
2022-10-05 15:07:25 -0500

    Update test file

--- src/__test__/3display.test.js
+++ src/__test__/3display.test.js
@@ -3,8 +3,8 @@ import displayTasks from "../modules/3display";
 
 test('Add one new item to the list', () => {
     document.body.innerHTML = '<input type="text" name="task" class="text-field full" placeholder="Add to your list..." required>' + '<ul class="task-list">' + '</ul>'
-    /* addTask("Hello"); */
+    addTask("Hello");
     displayTasks();
-    const list = document.querySelectorAll('.task-list');
-    expect(list).toHaveLength(2);
+    const list = document.querySelectorAll('textarea');
+    expect(list).toHaveLength(1);
 });
