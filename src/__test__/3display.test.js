47e30653c465e53dc2d47f8a360a18e33e2cceeb
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-06 12:45:13 -0500

    Add checkbox and clear tests

--- src/__test__/3display.test.js
+++ src/__test__/3display.test.js
@@ -1,8 +1,10 @@
 import addTask from '../modules/2add';
 import displayTasks from '../modules/3display';
+import clearTasks from '../modules/4clear';
+
+document.body.innerHTML = '<input type="text" name="task" class="text-field full" placeholder="Add to your list..." required> <ul class="task-list"> </ul> <div class="box clr-all">Clear all completed</div>';
 
 describe('Add task', () => {
-  document.body.innerHTML = '<input type="text" name="task" class="text-field full" placeholder="Add to your list..." required> <ul class="task-list"> </ul>';
   test('Should add one element when is called', () => {
     addTask('Hello');
     displayTasks();
@@ -12,7 +14,6 @@ describe('Add task', () => {
 });
 
 describe('Delete task', () => {
-
   test('Should remove an added task', () => {
     const deleteBtn = document.querySelector('.delete');
     deleteBtn.click();
@@ -20,3 +21,50 @@ describe('Delete task', () => {
     expect(listElement).toHaveLength(0);
   });
 });
+
+describe('Edit task', () => {
+
+  test('Should edit text when clicked', () => {
+    addTask('Hello');
+    displayTasks();
+    const taskItem = document.querySelector('li');
+    const taskForm = taskItem.querySelector('textarea');
+    taskForm.click(e => {
+      new FormData(e.target = 'helooooooo');
+    });
+    const locStorage = localStorage.getItem('taskArr');
+
+    displayTasks();
+    expect(locStorage).toEqual(JSON.stringify([{
+      index: 1,
+      description: 'Hello',
+      completed: false,
+    }]));
+  })
+
+});
+
+describe('Checkbox', () => {
+  test('Expect to change completed to tru after click', () => {
+    const checkBox = document.querySelector('.checkbox');
+    checkBox.click();
+    const locStorage = localStorage.getItem('taskArr');
+
+    expect(locStorage).toEqual(JSON.stringify([{
+      index: 1,
+      description: 'Hello',
+      completed: true,
+    }]));
+  })
+});
+
+describe('Clear all completed', () => {
+  test('Should delete all the tasks marked as completed', () => {
+    const list = document.querySelectorAll('textarea');
+    addTask('Wooooorld');
+    clearTasks();
+    displayTasks();
+    expect(list).toHaveLength(1);
+  })
+})
+
