7910414ba7204a1ef0e7c65f39cfec40cc2a6e92
Alejandra Keber <alejandrakeber@gmail.com>
2022-10-06 08:34:09 -0500

    Remove unnecesary code

--- src/__test__/3display.test.js
+++ src/__test__/3display.test.js
@@ -12,7 +12,6 @@ describe('Add task', () => {
 });
 
 describe('Delete task', () => {
-  document.body.innerHTML = '<input type="text" name="task" class="text-field full" placeholder="Add to your list..." required> <ul class="task-list"> <form class="task-form b-bottom box"> <input name="completed" type="checkbox" class="checkbox">; <textarea name="description" rows="1" class="task-text full" style="text-decoration:none">Hello</textarea> <button type="button" class="delete btn"> <i class="fa-solid fa-trash"></i> </button> <button type="submit" class="update btn"> <i class="fa-solid fa-pen"></i> </button> </form> </ul>';
 
   test('Should remove an added task', () => {
     const deleteBtn = document.querySelector('.delete');
